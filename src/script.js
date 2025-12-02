a7349c720778fa94ce43a0251103500c6516746a
JoshuaGit-Hub <150089591+JoshuaGit-Hub@users.noreply.github.com>
2025-12-02 14:03:45 -0600

    Revamp hero section and improve nav responsiveness
    Updated the hero section layout and content for clarity and visual appeal, including experience highlight and improved button text. Enhanced navigation responsiveness with overlay and close-on-click features, and added shadow effect to nav on scroll. Refactored CSS for new hero structure and overlay, and improved overall styling.

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-// Responsividade do NAV
+// Responsividade do NAV //
 
 const btnHamburguer = document.getElementById('menu-hamburguer');
 const navResponsiveContainer = document.getElementById('nav-responsive_container');
@@ -7,4 +7,40 @@ const navLinksResponsive = document.querySelector('#nav-links_responsive');
 btnHamburguer.addEventListener('click', () => {
     navResponsiveContainer.classList.toggle('active');
     navLinksResponsive.classList.toggle('active');
+    overlay.classList.toggle('active');
+});
+
+// Fechar o menu ao clicar em um link
+const navLinks = document.querySelectorAll('#nav-links_responsive a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navResponsiveContainer.classList.remove('active');
+        navLinksResponsive.classList.remove('active');
+        overlay.classList.remove('active');
+    });
+});
+
+// Fechar o menu ao clicar fora dele
+const overlay = document.getElementById('overlay');
+overlay.addEventListener('click', () => {
+    navResponsiveContainer.classList.remove('active');
+    navLinksResponsive.classList.remove('active');
+    overlay.classList.remove('active');
+});
+
+// Fim da responsividade do NAV //
+
+// Adicionar sombra ao NAV ao rolar a página //
+
+const navContainer = document.querySelector('.nav-container');
+const heroSection = document.getElementById('hero');
+let heightHero = heroSection.clientHeight;
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > heightHero) {
+        navContainer.classList.add('hero-passed');
+    }
+    else {
+        navContainer.classList.remove('hero-passed');
+    }
 });
